e99507724b1170c8e5abbf784a597712cecb53f0
Michael Hazani <mhazani@plutovr.com>
2021-03-08 15:17:44 -0800

    intro video, UE4 overlay extension tutorial, video embed plugin, new MAIT alias email

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,54 @@
-require("dotenv").config();
-const queries = require("./src/utils/algolia");
-const config = require("./config");
+require('dotenv').config();
+const queries = require('./src/utils/algolia');
+
+const config = require('./config');
+
 const plugins = [
   'gatsby-plugin-sitemap',
   'gatsby-plugin-sharp',
   {
     resolve: `gatsby-plugin-layout`,
     options: {
-        component: require.resolve(`./src/templates/docs.js`)
-    }
+      component: require.resolve(`./src/templates/docs.js`),
+    },
   },
   'gatsby-plugin-emotion',
   'gatsby-plugin-react-helmet',
   {
-    resolve: "gatsby-source-filesystem",
+    resolve: 'gatsby-source-filesystem',
     options: {
-      name: "docs",
-      path: `${__dirname}/content/`
-    }
+      name: 'docs',
+      path: `${__dirname}/content/`,
+    },
   },
   {
     resolve: 'gatsby-plugin-mdx',
     options: {
       gatsbyRemarkPlugins: [
         {
-          resolve: "gatsby-remark-images",
+          resolve: 'gatsby-remark-images',
           options: {
             maxWidth: 1035,
-            sizeByPixelDensity: true
-          }
+            sizeByPixelDensity: true,
+          },
         },
         {
-          resolve: 'gatsby-remark-copy-linked-files'
-        }
+          resolve: 'gatsby-remark-copy-linked-files',
+        },
+        {
+          resolve: 'gatsby-remark-better-embed-video',
+          options: {
+            width: 800,
+            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77.
+            height: 400, // Optional: Overrides optional.ratio.
+            related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
+            noIframeBorder: true, // Optional: Disable insertion of <style> border: 0.
+            showInfo: false, // Optional: Hides video title and player actions.
+          },
+        },
       ],
-      extensions: [".mdx", ".md"]
-    }
+      extensions: ['.mdx', '.md'],
+    },
   },
   {
     resolve: `gatsby-plugin-gtag`,
@@ -50,7 +63,12 @@ const plugins = [
   },
 ];
 // check and add algolia
-if (config.header.search && config.header.search.enabled && config.header.search.algoliaAppId && config.header.search.algoliaAdminKey) {
+if (
+  config.header.search &&
+  config.header.search.enabled &&
+  config.header.search.algoliaAppId &&
+  config.header.search.algoliaAdminKey
+) {
   plugins.push({
     resolve: `gatsby-plugin-algolia`,
     options: {
@@ -58,14 +76,14 @@ if (config.header.search && config.header.search.enabled && config.header.search
       apiKey: config.header.search.algoliaAdminKey, // algolia admin key to index
       queries,
       chunkSize: 10000, // default: 1000
-    }}
-  )
+    },
+  });
 }
 // check and add pwa functionality
 if (config.pwa && config.pwa.enabled && config.pwa.manifest) {
   plugins.push({
-      resolve: `gatsby-plugin-manifest`,
-      options: {...config.pwa.manifest},
+    resolve: `gatsby-plugin-manifest`,
+    options: { ...config.pwa.manifest },
   });
   plugins.push({
     resolve: 'gatsby-plugin-offline',
@@ -82,6 +100,34 @@ if (config.gatsby && !config.gatsby.trailingSlash) {
   plugins.push('gatsby-plugin-remove-trailing-slashes');
 }
 
+// gatsby-embed-video
+plugins.push({
+  resolve: 'gatsby-transformer-remark',
+  options: {
+    plugins: [
+      {
+        resolve: 'gatsby-remark-embed-video',
+        options: {
+          width: 800,
+          ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+          height: 400, // Optional: Overrides optional.ratio
+          related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+          noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+          loadingStrategy: 'lazy', //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
+          urlOverrides: [
+            {
+              id: 'youtube',
+              embedURL: videoId => `https://www.youtube-nocookie.com/embed/${videoId}`,
+            },
+          ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+          containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+          iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+        },
+      },
+    ],
+  },
+});
+
 module.exports = {
   pathPrefix: config.gatsby.pathPrefix,
   siteMetadata: {
@@ -90,7 +136,10 @@ module.exports = {
     docsLocation: config.siteMetadata.docsLocation,
     ogImage: config.siteMetadata.ogImage,
     favicon: config.siteMetadata.favicon,
-    logo: { link: config.header.logoLink ? config.header.logoLink : '/', image: config.header.logo }, // backwards compatible
+    logo: {
+      link: config.header.logoLink ? config.header.logoLink : '/',
+      image: config.header.logo,
+    }, // backwards compatible
     headerTitle: config.header.title,
     githubUrl: config.header.githubUrl,
     helpUrl: config.header.helpUrl,
@@ -98,5 +147,5 @@ module.exports = {
     headerLinks: config.header.links,
     siteUrl: config.gatsby.siteUrl,
   },
-  plugins: plugins
+  plugins: plugins,
 };
