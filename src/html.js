c33faacc1eb9819d1d7218ff3e8ab9b3675580b2
Michael Hazani <mhazani@plutovr.com>
2021-03-08 18:05:05 -0800

    cleanup, remove three as dep

--- src/html.js
+++ src/html.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import config from '../config';
-import * as THREE from "three";
 export default class HTML extends React.Component {
   render() {
     return (
