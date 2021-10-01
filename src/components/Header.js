cace944cf5d5185e6b30287cd552c5c30b1b438d
Ryan James <ryan@plutovr.com>
2021-10-01 14:43:47 -0700

    Embed logo in repo

--- src/components/Header.js
+++ src/components/Header.js
@@ -76,7 +76,7 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
       }
     `}
     render={data => {
-      const logoImg = require('./images/logo.svg');
+      const logoImg = require('./images/pluto-logo.png');
 
       const twitter = require('./images/twitter.svg');
 
